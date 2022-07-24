181331fa952e661264199a6a5220bad40fef905f
xtf0214 <2979283542@qq.com>
2022-07-25 00:49:10 +0800

    codeforces 810

---
+++ match/Codeforces/810/B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 10000 + 5;
+int main()
+{
+    int T, n, m;
+    cin >> T;
+    while (T--)
+    {
+        cin >> n >> m;
+        vector<int> a(n);
+    }
+    return 0;
+}
