4e39139c834f4bf5077029ab63c3f521a21dcef4
xtf0214 <2979283542@qq.com>
2022-07-30 17:17:38 +0800

    cpp迭代器

---
+++ CPP_learn/iterator.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 10000 + 5;
+int main()
+{
+    vector<int> arr(10, 1);
+    copy(arr.begin(), arr.end(), ostream_iterator<int>(cout, " "));
+}
