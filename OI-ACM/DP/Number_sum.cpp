9a2f109d087ed73ac0d7195f0b8d38cb157d1584
xtf0214 <2979283542@qq.com>
2022-08-13 15:29:11 +0800

    dp

---
+++ OI-ACM/DP/Number_sum.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e6 + 5;
+int main()
+{
+    return 0;
+}
--- OI-ACM/DP/full-bag.cpp
+++ OI-ACM/DP/full-bag.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
 using namespace std;
-const int maxn = 1e4 + 5;
-const int maxW = 1e7 + 5;
-int n, W, w[maxn], v[maxn];
-long long f[maxW];
-
+const int N = 1e3 + 5;
+int n, W, w[N], v[N];
+long long f[N];
+// long long dp[N][N];
+// void solve1()
+// {
+//     for (int i = 1; i <= n;i++)
+//         for (int j = 0; j <= W;j++)
+//             for (int k = 0; k * w[i] <= j;k++)
+//                 dp[i][j] = max(dp[i][j], dp[i - 1][j - k * w[i]] + v[i]);
+// }
+// void solve2()
+// {
+//     for (int i = 1; i <= n;i++)
+//         for (int j = w[i]; j <= W;j++)
+//             dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i]);
+// }
+long long dp[N];
+void solve3()
+{
+    for (int i = 1; i <= n; i++)
+        for (int j = w[i]; j <= W; j++)
+            dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
+}
 int main()
 {
     cin >> W >> n;
     for (int i = 1; i <= n; i++)
         cin >> w[i] >> v[i];
-    for (int i = 1; i <= n; i++)
-        for (int j = w[i]; j <= W; j++)
-            f[j] = max(f[j], f[j - w[i]] + v[i]); // 核心状态方程
-    cout << f[W];
+    solve3();
+    cout << dp[W];
     return 0;
 }
