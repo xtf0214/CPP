8593f294180f94e52bcbf63b524ef91cd76ed2e2
xtf0214 <2979283542@qq.com>
2022-09-19 10:44:57 +0800

    萌新联赛1

--- match/memgxin/1/f.cpp
+++ match/memgxin/1/f.cpp
@@ -1,22 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 using pii = pair<int, int>;
-const int MAXN = 10000 + 5;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+#define fi first
+#define se second
+#define all(v) v.begin(), v.end()
+#define print(v) for (int i = 0; i < v.size(); cout << v[i] << " \n"[i == v.size() - 1], ++i)
+const int N = 1e6 + 5, INF = 0x3f3f3f3f, mod = 1e9 + 7;
+
 int main()
 {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n, m, t;
-    int ans = 0;
-    pii a[MAXN];
     cin >> n >> m >> t;
+    priority_queue<int> pq;
+    vector<pii> a(m);
     for (int i = 0; i < m; i++)
-        cin >> a[i].first >> a[i].second;
-    sort(a, a + m);
-    for (int i = 0; i < m; i++)
-        if (a[i + 1].first >= t)
+        cin >> a[i].fi >> a[i].se;
+    sort(all(a));
+    int ans = 0, st = 0;
+    while (st == m || t < a.back().fi)
+    {
+        pii max;
+        for (int i = st; /* i < m && a[i].fi <= t ? true : st = i, false */; i++)
         {
-            t += a[i].second;
-            ans++;
+            if (i >= m || a[i].fi > t)
+            {
+                st = i;
+                break;
+            }
+            if (max.se < a[i].se)
+                max = a[i];
         }
+        t = max.fi + max.se;
+        ans++;
+    }
     cout << ans << endl;
     return 0;
 }
