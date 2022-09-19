ab63b14c3142004e037436ad77142b2917cb1025
xtf0214 <2979283542@qq.com>
2022-09-19 10:45:29 +0800

    leetcode

---
+++ OJ/Leetcode/2.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=cpp
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
+    {
+        ListNode *s;
+        for (ListNode *it = s; l1->next != nullptr; l1 = l1->next, l2 = l2->next, it = it->next)
+            it = new ListNode(l1->val + l2->val);
+        return s;
+    }
+};
+int main()
+{
+    
+}
+// @lc code=end
