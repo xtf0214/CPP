a8d15b6dd4955eb89f239b2d1f84358d3dfd17d7
tanphoon <2979283542@qq.com>
2022-10-23 16:55:58 +0800

    爆改洛谷文件结构

---
+++ OJ/Luogu/Training/201【算法2-2】线性复杂度优化与离散化/P1884.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+#define fi first
+#define se second
+#define all(v) v.begin(), v.end()
+#define print(v) for (int i = 0; i < v.size(); cout << v[i] << (char[2][2]){" ", "\n"}[i == v.size() - 1], ++i)
+const int N = 1e6 + 5, INF = 0x3f3f3f3f, mod = 1e9 + 7;
+
+vector<int[4]> a;
+void solve()
+{
+    
+}
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int T = 1;
+    // cin >> T;
+    while (T--)
+        solve();
+    a.push_back((int[4]){1, 2, 3, 4});
+    for(auto [x1,y1,x2,y2]:a)
+        cout << x1 << y1 << x2 << y2;
+    return 0;
+}
