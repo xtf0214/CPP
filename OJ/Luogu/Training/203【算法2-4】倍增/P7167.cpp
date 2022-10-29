282d33064a309d6422ca8417fa182b5a759e8901
tanphoon <2979283542@qq.com>
2022-10-29 18:54:26 +0800

    luogu

--- OJ/Luogu/Training/203【算法2-4】倍增/P7167.cpp
+++ OJ/Luogu/Training/203【算法2-4】倍增/P7167.cpp
@@ -1,8 +1,10 @@
+// P7167 [eJOI2020 Day1] Fountain
+// https://www.luogu.com.cn/problem/P7167
 #include <bits/stdc++.h>
 using namespace std;
 using vi = vector<int>;
 using vvi = vector<vi>;
-const int N = 1e6 + 5, INF = 0x3f3f3f3f, mod = 1e9 + 7;
+const int N = 1e6 + 5, INF = 0x3f3f3f3f;
 
 int main()
 {
@@ -26,27 +28,29 @@ int main()
         f[i][0] = next[i];
         g[i][0] = c[f[i][0]];
     }
+    // f[i][j]：从i节点走2^j步所到达的节点
+    // g[i][j]：从i节点出发，第1步到第2^(j-1)步经过的节点容量之和
     for (int t = 1; t <= 16; t++)
         for (int i = 1; i <= n; i++)
         {
-            int k = f[i][t - 1]; // 从
-            f[i][t] = f[k][t - 1];
+            int k = f[i][t - 1]; // 从i出发走2^(t-1)步
+            f[i][t] = f[k][t - 1]; 
             g[i][t] = g[i][t - 1] + g[k][t - 1];
         }
     while (m--)
     {
         int r, v;
         cin >> r >> v;
-        if (v > c[r])
+        if (v > c[r]) 
         {
             v -= c[r];
             for (int t = 16; t >= 0; t--)
-                if (v > g[r][t])
+                if (v > g[r][t]) // 只有当剩余容量大于g[r][t]才选择水流向下，避免v流完==0的情况
                 {
                     v -= g[r][t];
                     r = f[r][t];
                 }
-            r = f[r][0];
+            r = f[r][0]; 
         }
         if (r == n + 1)
             r = 0;
