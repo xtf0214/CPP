f6f01d9cdb11785154a3fbdd45d4bf1bbf00b1fc
Tanphoon <2979283542@qq.com>
2022-11-26 00:16:07 +0800

    PTA L2 7-94 出栈序列的合法性

--- OJ/PTA/L2/94.cpp
+++ OJ/PTA/L2/94.cpp
@@ -1,37 +1,29 @@
 // 7-94 出栈序列的合法性
 #include <bits/stdc++.h>
 using namespace std;
+int m, n, k;
+bool check(int a[])
+{
+    stack<int> s;
+    for (int x = 1, top = 0; x <= n; x++)
+    {
+        s.push(x);
+        if (s.size() > m)
+            return false;
+        while (!s.empty() && s.top() == a[top])
+            s.pop(), top++;
+    }
+    return s.empty();
+}
 int main()
 {
-    int m, n, k;
+
     cin >> m >> n >> k;
     for (int i = 0; i < k; i++)
     {
         int a[n];
-        stack<int> s;
-        int x = 1;
-        bool is_right = true;
         for (int i = 0; i < n; i++)
             cin >> a[i];
-        for (int j = 0; j < n;)
-        {
-            while (x <= a[j])
-            {
-                s.push(x++);
-                if (s.size() > m)
-                    is_right = false;
-            }
-            if (s.top() != a[j])
-            {
-                is_right = false;
-                break;
-            }
-            else
-            {
-                s.pop();
-                j++;
-            }
-        }
-        cout << (is_right ? "YES" : "NO") << endl;
+        cout << (check(a) ? "YES" : "NO") << endl;
     }
 }
