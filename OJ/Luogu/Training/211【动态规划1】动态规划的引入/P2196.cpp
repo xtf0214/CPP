f1abd1f57101039fb9d49901c78e5cbdfdad215c
Tanphoon <2979283542@qq.com>
2022-12-19 21:56:52 +0800

    Luogu P2196 挖地雷

--- OJ/Luogu/Training/211【动态规划1】动态规划的引入/P2196.cpp
+++ OJ/Luogu/Training/211【动态规划1】动态规划的引入/P2196.cpp
@@ -1,68 +1,40 @@
-// 	[NOIP1996 提高组] 挖地雷
+// P2196 挖地雷
 // https://www.luogu.com.cn/problem/P2196
 #include <bits/stdc++.h>
 using namespace std;
-
-using ll = long long;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using vi = vector<int>;
-using vll = vector<ll>;
-using vvi = vector<vi>;
-using vvll = vector<vll>;
-#define all(v) v.begin(), v.end()
-#define fi first
-#define se second
-template <typename T>
-void print(T &v)
-{
-    for (int i = 0; i < v.size(); i++)
-        cout << v[i] << " \n"[i == v.size() - 1];
-}
-template <typename T>
-void print(T *bg, T *ed)
-{
-    for (T *it = bg; it != ed; it++)
-        cout << *it << " \n"[it == ed - 1];
-}
-const int N = 1e3 + 5, INF = 0x3f3f3f3f, mod = 1e9 + 7;
-
-int n, a[N], cost[N][N], pre[N];
-int dp[N];
-void solve()
-{
-    for (int v = 1; v <= n; v++)
-    {
-        dp[v] = a[v];
-        for (int u = 1; u < v; u++)
-            if (cost[u][v])
-                if (dp[v] < dp[u] + a[v])
-                {
-                    dp[v] = dp[u] + a[v];
-                    pre[v] = u;
-                }
-    }
-}
-void printPath(int k)
-{
-    if (k == 0)
+int n, ans;
+vector<vector<int>> G;
+vector<int> w, dp, pre;
+void printPath(int u) {
+    if (u == 0)
         return;
-    printPath(pre[k]);
-    cout << k << " ";
+    printPath(pre[u]);
+    cout << u << " ";
 }
-int main()
-{
-    // ios::sync_with_stdio(0);
+void rec() {
+    vector<int> dp = w;
+    pre.resize(n + 1);
+    for (int v = 1; v <= n; v++) {
+        for (int u = 1; u < v; u++)
+            if (G[u][v] && dp[v] < dp[u] + w[v]) {
+                dp[v] = dp[u] + w[v];
+                pre[v] = u;
+            }
+        if (dp[ans] < dp[v])
+            ans = v;
+    }
+    printPath(ans);
+    cout << endl << dp[ans];
+}
+int main() {
     cin >> n;
+    G.assign(n + 1, vector<int>(n + 1));
+    w.resize(n + 1);
     for (int i = 1; i <= n; i++)
-        cin >> a[i];
+        cin >> w[i];
     for (int i = 1; i <= n; i++)
         for (int j = i + 1; j <= n; j++)
-            cin >> cost[i][j];
-    solve();
-    // print(dp + 1, dp + n + 1);
-    printPath(max_element(dp + 1, dp + n + 1) - dp);
-    cout << endl;
-    cout << *max_element(dp + 1, dp + n + 1) << endl;
+            cin >> G[i][j];
+    rec();
     return 0;
 }
