8a1a577f7d9e8c3ddd89a69bf49c2fb654e0dc35
Tanphoon <2979283542@qq.com>
2022-12-19 23:21:42 +0800

    Luogu P4017 最大食物链总数

--- OJ/Luogu/Training/211【动态规划1】动态规划的引入/P4017.cpp
+++ OJ/Luogu/Training/211【动态规划1】动态规划的引入/P4017.cpp
@@ -1,26 +1,41 @@
+// P4017 最大食物链总数
+// https://www.luogu.com.cn/problem/P4017
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-using vi = vector<int>;
-using vll = vector<ll>;
-using vvi = vector<vi>;
-using vvll = vector<vll>;
-#define fi first
-#define se second
-#define all(v) v.begin(), v.end()
-#define print(v) for (int i = 0; i < v.size(); cout << v[i] << " \n"[i == v.size() - 1], ++i)
-const int N = 1e6 + 5, INF = 0x3f3f3f3f, mod = 1e9 + 7;
-
-
-int n, m;
-int cost[N][N];
-int main()
-{
+const int mod = 80112002;
+int V, E;
+vector<vector<int>> G;
+vector<int> indeg, oudeg, dp;
+int ans;
+int dfs(int u) {
+    if (dp[u])
+        return dp[u];
+    int res = 0;
+    for (int v : G[u])
+        res = (res + dfs(v)) % mod;
+    return dp[u] = res;
+}
+int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-    cin >> n >> m;
-    for (int i = 0, u, v; i < m;i++)
-        cin >> u >> v, cost[u][v] = 1;
+    cin >> V >> E;
+    G.resize(V + 1);
+    indeg.resize(V + 1);
+    oudeg.resize(V + 1);
+    dp.resize(V + 1);
+    for (int i = 0, u, v; i < E; i++) {
+        cin >> u >> v;
+        G[v].push_back(u);
+        oudeg[u]++;
+        indeg[v]++;
+    }
+    for (int i = 1; i <= V; i++)
+        if (indeg[i] == 0)
+            dp[i] = 1;
+    for (int i = 1; i <= V; i++)
+        dfs(i);
+    for (int i = 1; i <= V; i++)
+        if (oudeg[i] == 0)
+            ans = (ans + dp[i]) % mod;
+    cout << ans;
     return 0;
 }
