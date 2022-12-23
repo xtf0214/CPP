314fd7d21c6300b1bc7e476e1dd5bc4bc4ed8d38
Tanphoon <2979283542@qq.com>
2022-12-23 22:35:18 +0800

    LCS问题

--- OI-ACM/DP/Linear/LCS.cpp
+++ OI-ACM/DP/Linear/LCS.cpp
@@ -1,23 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1e3 + 5;
 int n, m;
 string s, t;
-int dp[N][N];
-void solve()
-{
+int solve() {
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1));
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++)
             if (s[i - 1] == t[j - 1])
                 dp[i][j] = dp[i - 1][j - 1] + 1;
             else
                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+    return dp[n][m];
 }
-int main()
-{
+int main() {
     cin >> n >> m;
     cin >> s >> t;
-    solve();
-    cout << dp[n][m] << endl;
+    cout << solve() << endl;
     return 0;
 }
