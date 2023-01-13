a5d947e2483ab281cb94f70e85b0584a31b89113
Tanphoon <2979283542@qq.com>
2023-01-13 09:31:00 +0800

    Luogu P1004 [NOIP2000 提高组] 方格取数 优化

--- Problem/Luogu/Training/212【动态规划2】线性状态动态规划/P1004.cpp
+++ Problem/Luogu/Training/212【动态规划2】线性状态动态规划/P1004.cpp
@@ -2,7 +2,7 @@
  * @file    :   P1004 [NOIP2000 提高组] 方格取数
  * @author  :   Tanphoon
  * @date    :   2023/01/12 17:43
- * @version :   2023/01/12 17:43
+ * @version :   2023/01/13 09:28
  * @link    :   https://www.luogu.com.cn/problem/P1004
  */
 #include <bits/stdc++.h>
@@ -12,8 +12,8 @@ const int N = 2e2 + 5, INF = 0x3f3f3f3f;
 int f[2 * N][N][N], a[N][N], n;
 int main() {
     cin >> n;
-    for (int y = 1, x = 1, k = 1; y != 0 && x != 0 && k != 0;)
-        cin >> y >> x >> k, a[y][x] = k;
+    for (int y, x, k; cin >> y >> x >> k && y != 0 && x != 0 && k != 0;)
+        a[y][x] = k;
     for (int s = 2; s <= n + n; s++)
         for (int i = max(s - n, 1); i <= min(n, s - 1); i++)
             for (int k = max(s - n, 1); k <= min(n, s - 1); k++) {
