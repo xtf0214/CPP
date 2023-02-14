9398227ee8bb93f9eeb7f4f0f907e9b4381e6e5e
Tanphoon <2979283542@qq.com>
2023-02-14 12:20:20 +0800

    线性代数 伴随矩阵

--- OI-ACM/Math/LinearAlgebra/Inverse.cpp
+++ OI-ACM/Math/LinearAlgebra/Inverse.cpp
@@ -31,20 +31,32 @@ int det(const matrix A, int y = 0, int x = 0) {
         s += sign(i) * A[yy[i]][xx[0]] * det(A, bitSet1(y, yy[i]), bitSet1(x, xx[0]));
     return s;
 }
-matrix inverse(const matrix &A) {
+matrix adjoint(const matrix &A) {
     int n = A.size();
     matrix B(n, vector<int>(n));
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
-            B[i][j] = sign(i + j) * det(A, bitSet1(0, i), bitSet1(0, j));
+            B[j][i] = sign(i + j) * det(A, bitSet1(0, i), bitSet1(0, j));
     return B;
 }
-
+matrix operator*(const matrix &A, const matrix &B) {
+    matrix C(A.size(), vector<int>(B[0].size()));
+    for (int i = 0; i < A.size(); i++)
+        for (int k = 0; k < B.size(); k++)
+            for (int j = 0; j < B[0].size(); j++)
+                C[i][j] = (C[i][j] + A[i][k] * B[k][j]);
+    return C;
+}
 int main() {
     matrix A;
-    A = {{5, 7, 8}, {2, 3, 4}, {1, 6, 9}};
-    matrix B = inverse(A);
+    // A = {{5, 7, 8}, {2, 3, 4}, {1, 6, 9}};
+    A = {{1, 2, 3}, {2, 2, 1}, {3, 4, 3}};
+    matrix B = adjoint(A);
+    matrix C = A * B;
     printMatrix(B);
+    cout << endl;
+    printMatrix(C);
+    cout << endl;
     cout << det(A) << endl;
     return 0;
 }
