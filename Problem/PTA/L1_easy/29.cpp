b101e94935a66db5ed707172f2b872a92ee30aed
Tanphoon <2979283542@qq.com>
2023-03-02 21:40:38 +0800

    PTA L1简单 7-29 统计字符出现的次数

--- Problem/PTA/L1_easy/29.cpp
+++ Problem/PTA/L1_easy/29.cpp
@@ -1,51 +1,25 @@
-// 7-29 统计字符出现的次数
-#include <algorithm>
-#include <iostream>
+/**
+ * @file    :   7-29 统计字符出现的次数
+ * @author  :   Tanphoon
+ * @date    :   2023/03/02 18:38
+ * @version :   2023/03/02 18:38
+ */
+#include <bits/stdc++.h>
 using namespace std;
-struct Str
-{
-    int cnt, id;
-    char c;
-} a[60];
-bool cmp(const Str &a, const Str &b) { return a.id < b.id; }
-int main()
-{
-    string str;
-    getline(cin, str);
-    int n = 0;
-    for (int i = 0; i < 52; i++)
-        i < 26 ? a[i].c = i + 'a' : a[i].c = i - 26 + 'A';
-    for (int i = 0; i < str.size(); i++)
-    {
-        if ('A' <= str[i] && str[i] <= 'Z')
-        {
-            int j = str[i] - 'A' + 26;
-            a[j].cnt++;
-            if (a[j].id == 0)
-                a[j].id = i;
-        }
-        else if ('a' <= str[i] && str[i] <= 'z')
-        {
-            int j = str[i] - 'a';
-            a[j].cnt++;
-            if (a[j].id == 0)
-                a[j].id = i;
-        }
-        else
-            n++;
-    }
-    sort(a, a + 52, cmp);
+const int N = 128;
 
-    for (int i = 0, k = 0; i < 52; i++)
-    {
-        if (a[i].cnt != 0)
-        {
-            k % 10 == 0 ? printf("%c--%d", a[i].c, a[i].cnt) : printf(" %c--%d", a[i].c, a[i].cnt);
-            k++;
-            if (k % 10 == 0)
-                printf("\n");
-        }
+int main() {
+    string s;
+    while (getline(cin, s)) {
+        int cnt[N] = {0};
+        for (char ch : s)
+            cnt[ch]++;
+        vector<char> ans;
+
+        for (int ch = 0; ch < N; ch++)
+            if (isalpha(ch) && cnt[ch] > 0)
+                ans.push_back(ch);
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << "--" << cnt[ans[i]] << " \n"[(i + 1) % 10 == 0 || i + 1 == ans.size()];
     }
-    return 0;
 }
-// a bb ccc dddd ee ff gg hh ii jj kk ll mm nn oo pp qq rr ss tt uu vv
