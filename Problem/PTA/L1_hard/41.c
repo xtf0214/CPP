938017d45be8ea95394365ca706611db221a8406
Tanphoon <2979283542@qq.com>
2023-03-22 01:50:36 +0800

    PTA L1 7-41 二分法求多项式单根

--- Problem/PTA/L1_hard/41.c
+++ Problem/PTA/L1_hard/41.c
@@ -1,32 +1,25 @@
 // 7-41 二分法求多项式单根
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 float a0, a1, a2, a3;
-float f(float x) { return a3 * x * x * x + a2 * x * x + a1 * x + a0; }
-int main()
-{
-	float a, b;
-	scanf("%f %f %f %f", &a3, &a2, &a1, &a0);
-	scanf("%f %f", &a, &b);
-	float mid;
-	while (b - a > 0.0001)
-	{
-		if (f(a) == 0)
-		{
-			printf("%.2f", a);
-			return 0;
-		}
-		if (f(b) == 0)
-		{
-			printf("%.2f", b);
-			return 0;
-		}
-		mid = (a + b) / 2;
-		if (f(mid) * f(a) > 0)
-			a = mid;
-		else
-			b = mid;
-	}
-	printf("%.2f", mid);
-	return 0;
+double f(double x) { return a3 * x * x * x + a2 * x * x + a1 * x + a0; }
+
+double solve(double l, double r) {
+    while (r - l > 1e-6) {
+        double mid = (l + r) / 2;
+        if (f(mid) == 0)
+            return mid;
+        if (f(l) * f(mid) > 0)
+            l = mid;
+        else
+            r = mid;
+    }
+    return l;
+}
+
+int main() {
+    double a, b;
+    scanf("%lf %lf %lf %;f", &a3, &a2, &a1, &a0);
+    scanf("%lf %lf", &a, &b);
+    printf("%.2lf", solve(a, b));
 }
