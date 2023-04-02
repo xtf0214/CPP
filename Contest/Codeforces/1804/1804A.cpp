536564197bacd67e2bbf20b3836fa473e1d4a35a
Tanphoon <2979283542@qq.com>
2023-04-03 03:11:49 +0800

    CF Nebius Welcome Round (Div. 1 + Div. 2) A

---
+++ Contest/Codeforces/1804/1804A.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file    :   1804A
+ * @author  :   Tanphoon
+ * @date    :   2023/03/22 01:03
+ * @version :   2023/03/22 01:03
+ * @link    :   https://codeforces.com/contest/1804/problem/A
+ */
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define print(v) for (int i = 0; i < v.size(); cout << v[i] << " \n"[++i == v.size()])
+const int N = 1e6 + 5, INF = 0x3f3f3f3f, mod = 1e9 + 7;
+
+int solve() {}
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    int T = 1;
+    cin >> T;
+    while (T--)
+        cout << solve() << endl;
+    return 0;
+}
