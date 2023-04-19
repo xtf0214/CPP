5f11ebb5314b0c10c61381c82aae59a4740eb540
Tanphoon <2979283542@qq.com>
2023-04-20 03:25:46 +0800

    Luogu P2340 奶牛展览 滚动数组新用法

--- Problem/Luogu/Training/212【动态规划2】线性状态动态规划/P2340A.cpp
+++ Problem/Luogu/Training/212【动态规划2】线性状态动态规划/P2340A.cpp
@@ -1,8 +1,8 @@
 /**
- * @file    :   P2340
+ * @file    :   P2340 奶牛展览 [USACO03FALL]Cow Exhibition G
  * @author  :   Tanphoon
  * @date    :   2023/02/10 17:02
- * @version :   2023/02/10 17:02
+ * @version :   2023/04/20 03:23
  */
 #include <bits/stdc++.h>
 using namespace std;
@@ -22,18 +22,17 @@ vector<int> IQ, EQ;
 int solve() {
     vector f(2, NegaIdArray<int, -MR, MR>(-INF));
     f[0][0] = f[1][0] = 0;
-    int cr = 1;
-    for (int i = 1; i <= n; i++, cr ^= 1)
+    for (int i = 1; i <= n; i++)
         for (int j = -MR; j <= MR; j++) {
             if (-MR <= j - IQ[i] && j - IQ[i] <= MR)
-                f[cr][j] = max(f[cr ^ 1][j], f[cr ^ 1][j - IQ[i]] + EQ[i]);
+                f[i & 1][j] = max(f[(i - 1) & 1][j], f[(i - 1) & 1][j - IQ[i]] + EQ[i]);
             else
-                f[cr][j] = f[cr ^ 1][j];
+                f[i & 1][j] = f[(i - 1) & 1][j];
         }
     int ans = -INF;
     for (int i = 0; i <= MR; i++)
-        if (f[cr ^ 1][i] >= 0)
-            ans = max(ans, f[cr ^ 1][i] + i);
+        if (f[n & 1][i] >= 0)
+            ans = max(ans, f[n & 1][i] + i);
     return ans;
 }
 int main() {
