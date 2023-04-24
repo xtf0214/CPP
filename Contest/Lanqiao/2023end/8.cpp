b473a2bd28d41f61309e49d40015aaeb311f01b0
Tanphoon <2979283542@qq.com>
2023-04-24 21:11:43 +0800

    Lanqiao 2023 T8 整数删除

--- Contest/Lanqiao/2023end/8.cpp
+++ Contest/Lanqiao/2023end/8.cpp
@@ -1,22 +1,22 @@
+/** 
+ * @file    :   8 整数删除
+ * @author  :   Tanphoon 
+ * @date    :   2023/04/24 21:11
+ * @version :   2023/04/24 21:11
+ * @link    :   
+ */
 #include <bits/stdc++.h>
-
-#include "Timer.hpp"
-#include "_DEBUG.h"
 using namespace std;
-#define dbg(x) cerr << "\033[0;31m" << #x << " = " << x << ";\033[0m"
-
-typedef long long ll;
-typedef pair<int, int> pii;
+using ll = long long;
+using pli = pair<ll, int>;
 const int N = 5e5 + 5;
 ll a[N];
 int pre[N], nxt[N];
 int main() {
-    Timer timer;
-    timer.start();
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     int n, k;
     cin >> n >> k;
-    priority_queue<pii, vector<pii>, greater<pii>> q;
+    priority_queue<pli, vector<pli>, greater<pli>> q;
     for (int i = 1; i <= n; i++) {
         int x;
         cin >> x;
@@ -25,26 +25,25 @@ int main() {
         nxt[i] = i + 1;
         q.push({x, i});
     }
-    cout << timer.get().total << endl;
     while (k--) {
         while (a[q.top().second] != q.top().first) {
             q.pop();
         }
-        dbg(q.size());
-        int val = q.top().first, idx = q.top().second;
+        ll ai = q.top().first;
+        int i = q.top().second;
         q.pop();
-        int p1 = pre[idx];
-        int p2 = nxt[idx];
-        a[idx] = -1;
+        int p1 = pre[i];
+        int p2 = nxt[i];
+        a[i] = -1;
         pre[p2] = p1;
         nxt[p1] = p2;
 
         if (1 <= p1) {
-            a[p1] += val;
+            a[p1] += ai;
             q.push({a[p1], p1});
         }
         if (p2 <= n) {
-            a[p2] += val;
+            a[p2] += ai;
             q.push({a[p2], p2});
         }
     }
