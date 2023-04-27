89f4915df4b8ccb6f8a1790db8bf59038e223f72
Tanphoon <2979283542@qq.com>
2023-04-27 13:46:37 +0800

    OI-ACM 线段树代码的优化

--- OI-ACM/DataStructure/SegmentTree/0index.cpp
+++ OI-ACM/DataStructure/SegmentTree/0index.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-template <typename T, T (*op)(T, T), T e> struct SegmentTree {
+template <typename T, T (*op)(T, T), T (*e)()> class SegmentTree {
     int n;
     vector<T> dat;
     int ls(int p) { return p * 2 + 1; }
     int rs(int p) { return p * 2 + 2; }
-    SegmentTree(const vector<T> &v) : n(v.size()) {
-        n = 1 << (32 - __builtin_clz(n - 1));          // 补成满二叉树
-        dat.assign(2 * n, e);                          // 总共需要2n-1个节点
+    int bit_ceil(int n) { return 1 << 32 - __builtin_clz(n - 1); }
+
+  public:
+    SegmentTree(int len) : n(bit_ceil(len)), dat(2 * n, e()) {}
+    SegmentTree(const vector<T> &v) : n(bit_ceil(v.size())), dat(2 * n, e()) {
         copy(v.begin(), v.end(), dat.begin() + n - 1); // [0,n-2]为枝,[n-1,2*n-2]为叶
         for (int p = n - 2; p >= 0; p--)
             dat[p] = op(dat[ls(p)], dat[rs(p)]);
     }
     void update(int i, T k) { // 单点修改
         dat[i += n - 1] = k;
-        for (int p = (i - 1) / 2; p != -1; p = (p - 1) / 2)
-            dat[p] = op(dat[ls(p)], dat[(rs(p))]);
+        for (int p = (i - 1) >> 1; p != -1; p = (p - 1) >> 1)
+            dat[p] = op(dat[ls(p)], dat[rs(p)]);
     }
     T query(int a, int b) { return query(a, b, 0, 0, n); } // 区间查询[a,b)
     T query(int a, int b, int p, int l, int r) {
-        if (a >= r || b <= l)
-            return e;
-        if (a <= l && b >= r)
+        if (r <= a || b <= l)
+            return e();
+        if (a <= l && r <= b)
             return dat[p];
         int m = (l + r) / 2;
         T vl = query(a, b, ls(p), l, m);
@@ -30,12 +32,13 @@ template <typename T, T (*op)(T, T), T e> struct SegmentTree {
         return op(vl, vr);
     }
 };
-int _plus(int a, int b) { return a + b; }
+int Plus(int a, int b) { return a + b; }
+int e() { return 0; }
 int main() {
     int n;
     cin >> n;
     vector<int> arr(n, 1);
-    SegmentTree<int, _plus, 0> seg(arr);
+    SegmentTree<int, Plus, e> seg(arr);
     for (int i = 0; i < n; i++)
         seg.update(i, i);
     cout << seg.query(0, n) << endl;
