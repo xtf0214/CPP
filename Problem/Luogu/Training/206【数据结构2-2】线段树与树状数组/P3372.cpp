bc868b143a468b1af8276320da248f91de23a086
Tanphoon <2979283542@qq.com>
2023-05-06 01:04:54 +0800

    Luogu P3372 【模板】线段树 1

--- Problem/Luogu/Training/206【数据结构2-2】线段树与树状数组/P3372.cpp
+++ Problem/Luogu/Training/206【数据结构2-2】线段树与树状数组/P3372.cpp
@@ -1,39 +1,48 @@
+/**
+ * @file    :   P3372 【模板】线段树 1
+ * @author  :   Tanphoon
+ * @date    :   2023/05/05 23:37
+ * @version :   2023/05/05 23:37
+ * @link    :   https://www.luogu.com.cn/problem/P3372
+ */
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+
 template <typename T, T (*op)(T, T), T (*e)()> class SegmentTree {
     int n;
     vector<T> dat, tag;
-    int ls(int p) { return p << 1; }
-    int rs(int p) { return p << 1 | 1; }
     int bit_ceil(int n) { return 1 << 32 - __builtin_clz(n - 1); }
-    void mark(int p, int l, int r, int k) { l == r ? 0 : tag[p] += k, dat[p] += k * (r - l + 1); }
+    void mark(int k, int p, int l, int r) {
+        dat[p] += (r - l + 1) * k;
+        tag[p] += k;
+    }
     void push_down(int p, int l, int r) {
         if (tag[p]) {
             int m = (l + r) >> 1;
-            mark(ls(p), l, m, tag[p]);
-            mark(rs(p), m + 1, r, tag[p]);
+            mark(tag[p], p << 1, l, m);
+            mark(tag[p], p << 1 | 1, m + 1, r);
             tag[p] = 0;
         }
     }
 
   public:
-    SegmentTree(const vector<T> &v) : n(bit_ceil(v.size())), dat(2 * n, e()), tag(2 * n) {
+    SegmentTree(vector<T> &v) : n(bit_ceil(v.size())), dat(n << 1, e()), tag(n << 1) {
         copy(v.begin(), v.end(), dat.begin() + n);
-        for (int i = n - 1; i >= 1; i--)
-            dat[i] = op(dat[ls(i)], dat[rs(i)]);
+        for (int p = n - 1; p; p--)
+            dat[p] = op(dat[p << 1], dat[p << 1 | 1]);
     }
-    void update(int i, int k) { update(i, i, k, 1, 1, n); }
-    void update(int a, int b, int k) { update(a, b, k, 1, 1, n); }
+    void update(int a, int b, int k) { return update(a, b, k, 1, 1, n); }
     void update(int a, int b, int k, int p, int l, int r) {
         if (r < a || b < l)
             return;
         if (a <= l && r <= b)
-            return mark(p, l, r, k);
+            return mark(k, p, l, r);
         push_down(p, l, r);
         int m = (l + r) >> 1;
-        update(a, b, k, ls(p), l, m);
-        update(a, b, k, rs(p), m + 1, r);
-        dat[p] = op(dat[ls(p)], dat[rs(p)]);
+        update(a, b, k, p << 1, l, m);
+        update(a, b, k, p << 1 | 1, m + 1, r);
+        dat[p] = op(dat[p << 1], dat[p << 1 | 1]);
     }
     T query(int a, int b) { return query(a, b, 1, 1, n); }
     T query(int a, int b, int p, int l, int r) {
@@ -43,29 +52,29 @@ template <typename T, T (*op)(T, T), T (*e)()> class SegmentTree {
             return dat[p];
         push_down(p, l, r);
         int m = (l + r) >> 1;
-        T vl = query(a, b, ls(p), l, m);
-        T vr = query(a, b, rs(p), m + 1, r);
+        T vl = query(a, b, p << 1, l, m);
+        T vr = query(a, b, p << 1 | 1, m + 1, r);
         return op(vl, vr);
     }
 };
 template <typename T> T Add(T a, T b) { return a + b; }
 template <typename T> T e() { return 0; }
 int main() {
-    ios::sync_with_stdio(false);
     int n, m;
     cin >> n >> m;
-    vector<long long> arr(n);
-    for (int i = 0; i < n; ++i)
-        cin >> arr[i];
-    SegmentTree<long long, Add, e> seg(arr);
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    SegmentTree<ll, Add, e> seg(a);
     while (m--) {
-        int op, l, r, d;
+        int op, l, r, k;
         cin >> op >> l >> r;
         if (op == 1) {
-            cin >> d;
-            seg.update(l, r, d);
-        } else
+            cin >> k;
+            seg.update(l, r, k);
+        } else if (op == 2) {
             cout << seg.query(l, r) << '\n';
+        }
     }
     return 0;
 }
