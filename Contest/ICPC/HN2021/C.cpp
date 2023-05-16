978eb01f84e190a63aa0d0347672dc94bd5c687b
Tanphoon <2979283542@qq.com>
2023-05-16 21:18:19 +0800

    ICPC HN2021 C 尺取法、二分

--- Contest/ICPC/HN2021/C.cpp
+++ Contest/ICPC/HN2021/C.cpp
@@ -1,17 +1,26 @@
+/**
+ * @file    :   C   Alice and Bob
+ * @author  :   Tanphoon
+ * @date    :   2023/05/16 14:57
+ * @version :   2023/05/16 14:57
+ * @link    :   https://ac.nowcoder.com/acm/contest/17148/C
+ */
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
 const int N = 1e5 + 5;
 
-int a[N];
+int a[N], f[N];
+ll s[N];
 int n, m, k, cnt;
 map<int, int> mp;
-void add(int i) {
-    if (++mp[a[i]] == 1)
+
+void add(int x) {
+    if (++mp[a[x]] == 1)
         cnt++;
 }
-void del(int i) {
-    if (--mp[a[i]] == 0)
+void del(int x) {
+    if (--mp[a[x]] == 0)
         cnt--;
 }
 
@@ -19,12 +28,32 @@ int main() {
     cin >> n >> m >> k;
     for (int i = 1; i <= n; i++)
         cin >> a[i];
-    int ans = 0;
+    // 尺取法，f[i] 表示区间 [i, f[i]] 中有k个不同的数
+    for (int l = 1, r = 1; l <= n; l++) {
+        while (cnt < k && r <= n)
+            add(r++);
+        if (cnt == k)
+            f[l] = r - 1;
+        else
+            f[l] = n + 1;
+        del(l);
+    }
+    // 计算f的前缀和
+    for (int i = 1; i <= n; i++)
+        s[i] = s[i - 1] + f[i];
+
+    ll ans = 0;
     while (m--) {
-        int l, r;
-        cin >> l >> r;
-        l = min(l ^ ans, r ^ ans) + 1;
-        r = max(l ^ ans, r ^ ans) + 1;
-        
+        ll l1, r1;
+        cin >> l1 >> r1;
+        int l = min(l1 ^ ans, r1 ^ ans) + 1;
+        int r = max(l1 ^ ans, r1 ^ ans) + 1;
+        // 二分
+        ll p = upper_bound(f + 1, f + 1 + n, r) - f - 1;
+
+        ans = 0;
+        if (p >= l)
+            ans = (r + 1) * (p - l + 1) - (s[p] - s[l - 1]);
+        cout << ans << endl;
     }
 }
