12c5aa1046452e34e248057551afbe920d99feac
Tanphoon <2979283542@qq.com>
2023-05-18 02:40:20 +0800

    Nowcoder 萌新联赛2 D题 数对【树状数组】【离散化】【前缀和】

--- Contest/Nowcoder/2/A1.cpp
+++ Contest/Nowcoder/2/A1.cpp
@@ -1,47 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXN = 51;
-int G[MAXN][MAXN] = {0};
-int d1 = 3, d2 = 5;
-int vec[4][2] = {{d1, d2}, {d1, -d2}, {-d1, -d2}, {-d1, d2}};
+const int N = 51;
+int G[N][N];
+int dy = 3, dx = 5;
+int vec[4][2]{{dy, dx}, {dy, -dx}, {-dy, -dx}, {-dy, dx}};
 bool layer[100];
-int tx, ty;
-struct Node
-{
-    int x, y;
-    int st;
+struct Node {
+    int y, x, st;
 };
-inline void print()
-{
-    for (int i = 0; i < MAXN; i++)
-        for (int j = 0; j < MAXN; j++)
-            cout << (G[i][j] ? "  " : "[]") << "\n"[j != MAXN - 1];
+void print() {
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            cout << (G[i][j] ? "  " : "[]") << "\n"[j != N - 1];
     cout << endl;
 }
-int main()
-{
+int main() {
     queue<Node> q;
-    q.push({MAXN / 2, MAXN / 2, 0});
-    while (!q.empty())
-    {
+    q.push({N / 2, N / 2, 0});
+    while (!q.empty()) {
         Node t = q.front();
         q.pop();
-        if (!layer[t.st])
-        {
+        if (!layer[t.st]) {
             print();
             layer[t.st] = true;
             cin.get();
         }
         G[t.y][t.x] = 1;
-        for (int i = 0; i < 2; i++)
-            for (auto v : vec)
-            {
-                tx = t.x + v[i];
-                ty = t.y + v[i ^ 1];
-                if (tx < 0 || tx >= MAXN || ty < 0 || ty >= MAXN || G[ty][tx] == 1)
-                    continue;
-                q.push({ty, tx, t.st + 1});
-            }
+        for (auto &[ty, tx] : vec) {
+            int ny = t.y + ty, nx = t.x + tx;
+            if (nx < 0 || nx >= N || ny < 0 || ny >= N || G[ny][nx] == 1)
+                continue;
+            q.push({ny, nx, t.st + 1});
+        }
     }
     print();
     return 0;
--- Contest/Nowcoder/2/D.cpp
+++ Contest/Nowcoder/2/D.cpp
@@ -1,48 +1,55 @@
+/**
+ * @file    :   D 数对
+ * @author  :   Tanphoon
+ * @date    :   2023/05/18 01:59
+ * @version :   2023/05/18 01:59
+ * @link    :   https://ac.nowcoder.com/acm/contest/37344/D
+ */
 #include <bits/stdc++.h>
-#define lowbit(x) x & -x
 using namespace std;
 using ll = long long;
-const int MAX = 4e5 + 100;
-ll c[MAX];
-ll n, x, y;
-vector<ll> nums;
-int getSum(int x)
-{
-    int res = 0;
-    for (; x; x -= lowbit(x))
-        res += c[x];
-    return res;
-}
-void add(int x, int d)
-{
-    for (; x <= nums.size(); x += lowbit(x))
-        c[x] += d;
-}
-int query(int l, int r) { return getSum(r) - getSum(l - 1); }
-int get(ll x) { return lower_bound(nums.begin(), nums.end(), x) - nums.begin() + 1; }
-int main()
-{
+const int N = 2e5 + 5;
 
-    cin >> n >> x >> y;
-    int b[n + 1], sum_b[n + 1] = {0};
-    for (int i = 1; i <= n; i++)
-        cin >> b[i], b[i] -= y;
-    nums.push_back(0);
-    for (int i = 1; i <= n; i++)
-    {
-        sum_b[i] = sum_b[i - 1] + b[i];
-        nums.push_back(sum_b[i]);
-        nums.push_back(sum_b[i] - x);
+template <typename T> struct BIT {
+    int size;
+    vector<T> dat, arr;
+    BIT(int n = 0) : size(n), dat(n + 1), arr(n + 1) {}
+    inline int lowbit(int x) { return x & -x; }
+    void add(int i, T x) {
+        for (arr[i] += x; i <= size; i += lowbit(i))
+            dat[i] += x;
     }
-    // 离散化
-    sort(nums.begin(), nums.end());
-    nums.erase(unique(nums.begin(), nums.end()), nums.end());
-    add(get(0), 1);
-    ll ans = 0;
-    for (int i = 1; i <= n; i++)
-    {
-        ans += query(get(sum_b[i] - x), nums.size());
-        add(get(sum_b[i]), 1);
+    T get(int i) {
+        T res = 0;
+        for (; i; i -= lowbit(i))
+            res += dat[i];
+        return res;
+    }
+    T query(int l, int r) { return get(r) - get(l - 1); }
+};
+
+int main() {
+    int n, x, y;
+    cin >> n >> x >> y;
+    vector<int> b(n + 1);
+    vector<ll> prb(n + 1), d(1);
+    for (int i = 1; i <= n; i++) {
+        cin >> b[i];
+        b[i] -= y;
+        prb[i] = prb[i - 1] + b[i];
+        d.push_back(prb[i]);
+        d.push_back(prb[i] - x);
+    }
+    sort(d.begin(), d.end());
+    d.erase(unique(d.begin(), d.end()), d.end());
+    auto get = [&d](int x) { return lower_bound(d.begin(), d.end(), x) - d.begin() + 1; };
+    BIT<int> bit(d.size());
+    bit.add(get(prb[0]), 1);
+    ll ans = 0; 
+    // rep(r, 1, n)每次查询满足 prb[r] - x <= prb[l - 1] 的 l 的个数
+    for (int r = 1; r <= n; r++) {
+        ans += bit.query(get(prb[r] - x), d.size());
+        bit.add(get(prb[r]), 1);
     }
     cout << ans << endl;
     return 0;
