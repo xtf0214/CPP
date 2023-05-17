f572999ba981d88ffe064e15fe1c83ad34846ff8
Tanphoon <2979283542@qq.com>
2023-05-18 01:17:34 +0800

    Nowcoder 萌新联赛1 H题 兴奋值【二分】【ST表】

--- Contest/Nowcoder/1/h.cpp
+++ Contest/Nowcoder/1/h.cpp
@@ -1,85 +1,51 @@
-#include <algorithm>
-#include <iostream>
-#include <cstring>
-#include <cstdio>
-#include <cmath>
-#include <fstream>
+/**
+ * @file    :   h 兴奋值
+ * @author  :   Tanphoon
+ * @date    :   2023/05/17 23:30
+ * @version :   2023/05/17 23:30
+ * @link    :   https://ac.nowcoder.com/acm/contest/37160/H
+ */
+#include <bits/stdc++.h>
 using namespace std;
-
 using ll = long long;
+const int N = 2e5 + 5;
 
-const int N = 2e5 + 100;
-
-// 求max[l<=i<=r] { min(a_i,i - l + 1) }
-// 发现 i - l + 1 在一次询问中是单调递增的
-// 那么当我们确定答案mid,那么其实就变成查询[i + mid - 1,r]中max(a_i)
-// 因为这时候小于i + mid - 1 的位置受到mid的限制，肯定不会大于mid，那么只有后面
-// 通过 max(a_i) 是否大于mid就可以判断正确性了。
-// 所以我们使用二分+st表
-
-int n, m;
+template <typename T> struct ST {
+    int n, logn;
+    vector<vector<T>> dat;
+    ST(const vector<T> &v = {}) : n(v.size()), logn(__lg(n) + 1), dat(n + 1, vector<T>(logn + 1)) {
+        for (int j = 0; j <= logn; j++)
+            for (int i = 1; i + (1 << j) - 1 <= n; i++)
+                dat[i][j] = j ? max(dat[i][j - 1], dat[i + (1 << j - 1)][j - 1]) : v[i - 1];
+    }
+    T query(int l, int r) {
+        int s = __lg(r - l + 1);
+        return max(dat[l][s], dat[r - (1 << s) + 1][s]);
+    }
+};
 int a[N];
-int f[N][20]; // st表
-
-void init()
-{ // 初始化st表
-    for (int k = 0; k < 20; k++)
-    {
-        for (int i = 1; i + (1 << k) - 1 <= n; i++)
-        {
-            if (!k)
-                f[i][k] = a[i];
-            else
-                f[i][k] = max(f[i][k - 1], f[i + (1 << (k - 1))][k - 1]);
-        }
-    }
-}
-
-int get(int l, int r)
-{ // st表获得区间最大值
-    if (l > r)
-        return 0;
-    int len = r - l + 1;
-    int k = log(len) / log(2);
-    return max(f[l][k], f[r - (1 << k) + 1][k]);
-}
-
-bool check(int mid, int a, int b)
-{ // 二分检查函数
-    return get(a + mid - 1, b) >= mid;
-}
-
-int calc(int a, int b)
-{ // 进行二分
-    int l = 0, r = b - a + 1;
-    while (l < r)
-    {
-        int mid = l + r + 1 >> 1;
-        if (check(mid, a, b))
-            l = mid;
-        else
-            r = mid - 1;
-    }
-    return l;
-}
-std::ifstream fin("h.in");
-#define cin fin
-int main()
-{
-
+int main() {
+    int n, m;
     cin >> n >> m;
-
     for (int i = 1; i <= n; i++)
         cin >> a[i];
-
-    init(); // 初始化st表
-
-    while (m--)
-    {
+    ST<int> st(vector<int>(a + 1, a + 1 + n));
+    while (m--) {
         int a, b;
         cin >> a >> b;
-
-        printf("%d ", calc(a, b));
+        int l = 0, r = (b - a + 1) + 1, m;
+        auto check = [&](int m) { return st.query(a + m - 1, b) >= m; };
+        while (r - l > 1)
+            check(m = (l + r) / 2) ? l = m : r = m;
+        cout << l << " ";
     }
     return 0;
 }
+/*
+            l r
+      0[1 2 3 4 5]6
+2 3 2 8 2 4 1 2 9 4
+对于[a, a+l-1]有max[a, a+l-1]>=l,最大贡献为l
+对于[a+r-1, b]有max[a+r-1, b]< r,最大贡献小于r
+找出最大的l,即为ans
+ */
