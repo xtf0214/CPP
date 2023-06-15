d97069769f15ed1c9653f0269e5e16612353a10d
Tanphoon <2979283542@qq.com>
2023-06-15 22:19:50 +0800

    Nowcoder 萌新联赛3 I题 旅行 dijkstra

--- Contest/Nowcoder/3/I.cpp
+++ Contest/Nowcoder/3/I.cpp
@@ -1,51 +1,55 @@
+/**
+ * @file    :   I 旅行
+ * @author  :   Tanphoon
+ * @date    :   2023/06/15 21:36
+ * @version :   2023/06/15 21:36
+ * @link    :   https://ac.nowcoder.com/acm/contest/37782/I
+ */
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX_V = 1e5 + 5, INF = 0x3f3f3f3f;
-int V, E, x;
-struct Edge
-{
-    int to, cost;
-};
-vector<Edge> adjs[MAX_V];
+using ll = long long;
+using Node = pair<ll, int>;
 
-typedef pair<int, int> Node; // Node(int dist, int id)
-int d[MAX_V], pre[MAX_V];
+const int N = 2e5 + 5;
 
-void dijkstra(int s)
-{
-    priority_queue<Node, vector<Node>, greater<Node>> que; //默认对pair.first排序
-    fill(d, d + V, INF);
-    d[s] = 0, pre[s] = -1;
-    que.push({0, s});
-    while (!que.empty())
-    {
-        Node p = que.top();
-        que.pop();
-        int u = p.second;
-        if (d[u] < p.first)
+int n, m, x;
+vector<pair<int, int>> G[N];
+ll d[N];
+bool vis[N];
+
+void dijkstra(int s) {
+    priority_queue<Node, vector<Node>, greater<Node>> q;
+    memset(d, 0x3f, sizeof(d));
+    q.emplace(d[s] = 0, s);
+    while (!q.empty()) {
+        auto [d_u, u] = q.top();
+        q.pop();
+        if (vis[u])
             continue;
-        for (Edge e : adjs[u])
-            if (d[e.to] > d[u] + e.cost)
-                que.push({d[e.to] = d[u] + e.cost, e.to}), pre[e.to] = u;
+        vis[u] = true;
+        for (auto &[v, w] : G[u])
+            if (d[v] > d[u] + w)
+                q.emplace(d[v] = d[u] + w, v);
     }
 }
-int main()
-{
-    cin >> V >> E >> x;
-    // cin >> V >> E;
-    for (int i = 0, u, v, w; i < E; i++)
-    {
+
+int main() {
+    cin >> n >> m >> x;
+    while (m--) {
+        int u, v, w;
         cin >> u >> v >> w;
-        u--, v--;
-        adjs[u].push_back({v, w});
+        // u, v 是没做核酸的城市
+        // u+n, v+n 是做了核酸的城市
+        // 没做核酸的去做过核酸的，路径w+x
+        // 做过核酸的去没做过核酸的,路径w
+        G[u].emplace_back(v + n, w + x);
+        G[v + n].emplace_back(u, w);
+
+        G[v].emplace_back(u + n, w + x);
+        G[u + n].emplace_back(v, w);
     }
-    dijkstra(0);
-    int res = 0;
-    for (int i = 0; i < V;i++)
-        cout << d[i] << " ";
-    cout << endl;
-    for (int u = V - 1; u != 0; u = pre[u])
-        res++;
-    cout << d[V - 1] + res;
+    dijkstra(1);
+    ll ans = min(d[n], d[2 * n]);
+    cout << ans << '\n';
     return 0;
 }
