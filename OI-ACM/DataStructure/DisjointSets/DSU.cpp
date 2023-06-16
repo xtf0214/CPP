808d89128cbc1676c477a14295876ee32ce08e80
Tanphoon <2979283542@qq.com>
2023-06-16 19:23:32 +0800

    OI-ACM 加权并查集

---
+++ OI-ACM/DataStructure/DisjointSets/DSU.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct DSU {
+    vector<int> p, sz;
+    DSU(int n = 1e6) : p(n), sz(n, 1) { iota(p.begin(), p.end(), 0); }
+    int find(int x) { return x == p[x] ? x : p[x] = find(p[x]); }
+    void merge(int x, int y) { x = find(x), y = find(y), (x != y ? p[y] = x, sz[x] += sz[y] : 0); }
+    int size(int x) { return sz[find(x)]; }
+};
--- OI-ACM/DataStructure/DisjointSets/pathCompression.cpp
+++ OI-ACM/DataStructure/DisjointSets/pathCompression.cpp
@@ -4,7 +4,7 @@ struct DisjointSet {
     vector<int> p;
     DisjointSet(int n = 1e6) : p(n) { iota(p.begin(), p.end(), 0); }
     int find(int x) { return x == p[x] ? x : p[x] = find(p[x]); }
-    void unite(int x, int y) { find(x) != find(y) ? p[find(x)] = find(y) : 0; }
+    void unite(int x, int y) { p[find(x)] = find(y); }
 };
 int main() {
     int n, a, b, q, op;
