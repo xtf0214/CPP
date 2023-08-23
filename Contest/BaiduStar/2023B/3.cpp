5f7097a3bc77d6b476277dcb2c9a9ceb459560dd
Tanphoon <2979283542@qq.com>
2023-08-24 01:42:41 +0800

    百度之星2023第二场 C 迭代dp更新

--- Contest/BaiduStar/2023B/3.cpp
+++ Contest/BaiduStar/2023B/3.cpp
@@ -1,3 +1,10 @@
+/** 
+ * @file    :   3
+ * @author  :   Tanphoon 
+ * @date    :   2023/08/24 01:42
+ * @version :   2023/08/24 01:42
+ * @link    :   https://www.matiji.net/exam/brushquestion/11/4347/179CE77A7B772D15A8C00DD8198AAC74?from=1
+ */
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
@@ -13,13 +20,23 @@ signed main() {
         mp[a[i]] = i;
     }
     f[1] = 0;
-    for (int i = 1; i <= n; i++) {
-        if (i - 1 >= 1)
-            f[i - 1] = min(f[i - 1], f[i] + 1);
-        if (i + 1 <= n)
-            f[i + 1] = min(f[i + 1], f[i] + 1);
-        if (nxt[i])
-            f[nxt[i]] = min(f[nxt[i]], f[i] + 1);
+
+    int sum = -1;
+    while (true) {
+        int now = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i - 1 >= 1)
+                f[i - 1] = min(f[i - 1], f[i] + 1);
+            if (i + 1 <= n)
+                f[i + 1] = min(f[i + 1], f[i] + 1);
+            if (nxt[i])
+                f[nxt[i]] = min(f[nxt[i]], f[i] + 1);
+        }
+        for (int i = 1; i <= n; i++)
+            now += f[i];
+        if (now == sum)
+            break;
+        sum = now;
     }
     // print(f);
     cout << f[n] << '\n';
