9bb86459651e1e128eb230894d6515f118e1754c
Tanphoon <2979283542@qq.com>
2023-09-01 15:15:07 +0800

    DSU模板更新

--- OI-ACM/DataStructure/DisjointSets/DSU.cpp
+++ OI-ACM/DataStructure/DisjointSets/DSU.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 struct DSU {
+    int size;
     vector<int> p, sz;
-    DSU(int n = 1e6) : p(n), sz(n, 1) { iota(p.begin(), p.end(), 0); }
+    DSU(int n = 1e6) : size(n), p(n), sz(n, 1) { iota(p.begin(), p.end(), 0); }
     int find(int x) { return x == p[x] ? x : p[x] = find(p[x]); }
-    void merge(int x, int y) { x = find(x), y = find(y), (x != y ? p[y] = x, sz[x] += sz[y] : 0); }
-    int size(int x) { return sz[find(x)]; }
+    void merge(int x, int y) { x = find(x), y = find(y), (x != y ? p[y] = x, sz[x] += sz[y], --size : 0); }
+    int count(int x) { return sz[find(x)]; }
 };
