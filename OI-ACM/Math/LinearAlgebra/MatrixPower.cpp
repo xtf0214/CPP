3ed8bc0b8bb6a54d11733d33bb552259fdd0da13
Tanphoon <2979283542@qq.com>
2023-09-01 15:39:28 +0800

    矩阵乘法与矩阵快速幂

--- OI-ACM/Math/LinearAlgebra/MatrixPower.cpp
+++ OI-ACM/Math/LinearAlgebra/MatrixPower.cpp
@@ -1,35 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 10005;
-using matrix = vector<vector<int>>;
+
 using ll = long long;
-matrix operator*(matrix &A, matrix &B)
-{
-    matrix C(A.size(), vector<int>(B[0].size()));
+using Matrix = vector<vector<int>>;
+Matrix operator*(const Matrix &A, const Matrix &B) {
+    Matrix C(A.size(), vector<int>(B[0].size()));
     for (int i = 0; i < A.size(); i++)
         for (int k = 0; k < B.size(); k++)
             for (int j = 0; j < B[0].size(); j++)
                 C[i][j] = (C[i][j] + A[i][k] * B[k][j]);
     return C;
 }
-matrix pow(matrix &A, ll n)
-{
-    matrix B(A.size(), vector<int>(A[0].size()));
+Matrix pow(Matrix &A, ll n) {
+    Matrix B(A.size(), vector<int>(A[0].size()));
     for (int i = 0; i < B.size(); i++)
         B[i][i] = 1;
-    while (n)
-    {
+    for (; n; n >>= 1, A = A * A)
         if (n & 1)
             B = B * A;
-        A = A * A;
-        n >>= 1;
-    }
     return B;
 }
-int main()
-{
+int main() {
     ll n = 2;
-    matrix A(2, vector<int>(2));
+    Matrix A(2, vector<int>(2));
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             cin >> A[i][j];
