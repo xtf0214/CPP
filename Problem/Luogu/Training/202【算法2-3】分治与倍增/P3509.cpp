86b4c2128f1719929fce38d6cbbfbb5be571cb86
Tanphoon <2979283542@qq.com>
2024-09-24 21:01:40 +0800

    Luogu P3509 [POI2010]ZAB-Frog【倍增】

--- Problem/Luogu/Training/202【算法2-3】分治与倍增/P3509.cpp
+++ Problem/Luogu/Training/202【算法2-3】分治与倍增/P3509.cpp
@@ -1,22 +1,20 @@
-// P3509 [POI2010]ZAB-Frog
-// https://www.luogu.com.cn/problem/P3509
+/**
+ * @file    :   P3509 [POI2010]ZAB-Frog
+ * @author  :   Tanphoon
+ * @date    :   2024/09/24 20:37
+ * @version :   2024/09/24 20:37
+ * @link    :   https://www.luogu.com.cn/problem/P3509
+ */
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-using vi = vector<int>;
-using vll = vector<ll>;
-#define all(v) v.begin(), v.end()
-#define print(v) for (int i = 1; i < v.size(); cout << v[i] << " \n"[i++ == v.size() - 1])
-
 ll n, k, m;
-vll nxt, ans;
-vll a;
-void getKth(int k)
-{
+vector<ll> nxt, ans, a;
+
+void getKth(int k) {
     int l = 1, r = k + 1;
     nxt[1] = k + 1;
-    for (int i = 2; i <= n; i++)
-    {
+    for (int i = 2; i <= n; i++) {
         while (r + 1 <= n && a[i] - a[l] > a[r + 1] - a[i])
             l++, r++;
         if (a[i] - a[l] < a[r] - a[i])
@@ -25,45 +23,39 @@ void getKth(int k)
             nxt[i] = l;
     }
 }
-int main()
-{
+int main() {
     ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     cin >> n >> k >> m;
-    a = vll(n + 1);
-    nxt = vll(n + 1);
-    ans = vll(n + 1);
-    iota(all(ans), 0); // pos初始化为当前坐标
+    a = vector<ll>(n + 1);
+    nxt = vector<ll>(n + 1);
+    ans = vector<ll>(n + 1);
+    iota(ans.begin(), ans.end(), 0); // pos初始化为当前坐标
     for (int i = 1; i <= n; i++)
         cin >> a[i];
     getKth(k); // 尺取法获取第k近的点下标
-
-    // // 传统倍增模板
-    // // 初始化
-    // vector<vll> f(n + 1, vll(__lg(m) + 2));
-    // for (int i = 1; i <= n; i++)
-    //     f[i][0] = nxt[i];
-    // for (int t = 1; t <= __lg(m) + 1; t++)
-    //     for (int i = 1; i <= n; i++)
-    //         f[i][t] = f[f[i][t - 1]][t - 1];
-    // // 查询
-    // for (int t = 0; m; m >>= 1, t++)
-    //     if (m & (1 << t))
-    //     {
-    //         for (int i = 1; i <= n; i++)
-    //             pos[i] = f[pos[i]][t]; //从上次的位置接着跳
-    //     }
-
-    // 都什么年代了还在用传统模板，全新快速幂式模板
-    vll f(all(nxt));
-    for (; m; m >>= 1)
-    {
-        if (m & 1)
-            for (int i = 1; i <= n; i++)
-                ans[i] = f[ans[i]]; //从上次的位置接着跳
-        vll ff(f);
+    int logm = __lg(m);
+    vector f(n + 1, vector<ll>(logm + 1));
+    for (int i = 1; i <= n; i++)
+        f[i][0] = nxt[i];
+    for (int t = 1; t <= logm; t++)
         for (int i = 1; i <= n; i++)
-            f[i] = ff[ff[i]]; //倍增跳
-    }
-    print(ans);
+            f[i][t] = f[f[i][t - 1]][t - 1];
+    for (int t = logm; t >= 0; t--)
+        if (m - (1 << t) >= 0) {
+            for (int i = 1; i <= n; i++)
+                ans[i] = f[ans[i]][t];
+            m -= 1LL << t;
+        }
+    // vector<ll> f(nxt);
+    // for (; m; m >>= 1) {
+    //     if (m & 1)
+    //         for (int i = 1; i <= n; i++)
+    //             ans[i] = f[ans[i]]; // 从上次的位置接着跳
+    //     vector<ll> ff(f);
+    //     for (int i = 1; i <= n; i++)
+    //         f[i] = ff[ff[i]]; // 倍增跳
+    // }
+    for (int i = 1; i <= n; i++)
+        cout << ans[i] << ' ';
     return 0;
 }
